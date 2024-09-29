b6f0a7823288d53efe1ba5241a88cf64bc546ea3
AishMishra001 <aishlunatic001@gmail.com>
2024-09-29 07:19:56 +0530

    sol airdrop works

--- src/components/Airdrop.jsx
+++ src/components/Airdrop.jsx
@@ -1,19 +1,23 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useState } from "react";
 
 export function Airdrop(){
 
   const wallet = useWallet() ; 
   const  {connection} = useConnection()  ; 
+  const [ input , setInput ] = useState(0) ; 
 
 
   async function sendAirdroptoUser(){
-    await connection.requestAirdrop(wallet.publicKey , 1000000000); 
-    alert("1 sol Airdrop  sent to your wallet") ; 
+    await connection.requestAirdrop(wallet.publicKey , input*1000000000); 
+    alert( `${input} sol Airdrop  sent to your wallet`) ; 
   }
 
    return (
      <div>
-         <input type="text" placeholder="Amount" name="" id="" />
+         <input type="text" placeholder="Amount" value= {input} name="" id="" onChange={(e)=>{
+              setInput(e.target.value) ; 
+         }} />
          <button onClick={sendAirdroptoUser}>Send Airdrop</button>
          <p>{wallet.publicKey ? wallet.publicKey.toString()  : "No Wallet Connected"}</p>
      </div>
